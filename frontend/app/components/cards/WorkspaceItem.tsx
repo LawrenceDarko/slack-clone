d810a5086c772417c86fcc9f5d67f82afb0ce4a3
LawrenceDarko <darkolawrence@gmail.com>
2024-02-04 22:16:06 +0000

    workspace name symbol

--- frontend/app/components/cards/WorkspaceItem.tsx
+++ frontend/app/components/cards/WorkspaceItem.tsx
@@ -10,7 +10,7 @@ const WorkspaceItem: React.FC<WorkspaceItemProps> = ({spaceName, numOfMembers}) 
     return (
         <section className='flex justify-between w-full h-20 p-3 border-b cursor-pointer'>
             <div className='flex items-center justify-center gap-2'>
-                <div className='h-[95%] w-16 rounded-md bg-gray-200'></div>
+                <div className='h-[95%] w-16 rounded-md bg-gray-200 flex justify-center items-center text-4xl font-bold'>{spaceName[0]}</div>
                 <div>
                     <p>{spaceName}</p>
                     <p className='text-xs'>{numOfMembers} members</p>
