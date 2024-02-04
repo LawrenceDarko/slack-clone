4d641db23ef9f80173e97aa37911442cbb60d703
LawrenceDarko <darkolawrence@gmail.com>
2024-02-04 22:12:58 +0000

    invite registration route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllUsers, registerUser, loginUser, getAUser } from '../controllers/userController';
+import { getAllUsers, registerUser, loginUser, getAUser, loginUserWithInvitation, registerUserWithInvitation, sendEmailInvitation } from '../controllers/userController';
 
 const router = express.Router();
 
@@ -7,7 +7,10 @@ const router = express.Router();
 router.get('/', getAllUsers)
 router.get('/:userId', getAUser)
 router.post('/register', registerUser)
+router.post('/register-with-invitation', registerUserWithInvitation); 
 router.post('/login', loginUser)
+router.post('/login-with-invitation', loginUserWithInvitation);
+router.post('/send-invitation', sendEmailInvitation);
 
 
 export default router
