5a623f80aa5cc5427fba54bffb1ad7365fec63e2
LawrenceDarko <darkolawrence@gmail.com>
2024-02-06 18:53:48 +0000

    channel invite bug fix

--- frontend/app/components/Modals/Modal.tsx
+++ frontend/app/components/Modals/Modal.tsx
@@ -36,7 +36,7 @@ const Modal = () => {
                 created_by: user.id,
             }
             const response = await axiosInstance.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/channels/create`, channelData)
-            const responseData = response.data
+            const responseData = response?.data
             
             console.log(responseData)
         } catch (error) {
@@ -50,7 +50,7 @@ const Modal = () => {
 
     return (
         <div className={`${showModal === false ? 'hidden' : ''} fixed z-50 flex items-center inset-0 justify-center w-full h-full bg-black backdrop-blur-md bg-opacity-20`}>
-            <div className="w-[448px] bg-white rounded-md p-8">
+            <div className="w-[448px] bg-white dark:bg-[var(--dark-theme-bg-color)] rounded-md p-8">
                 <div className="flex justify-between w-full">
                     <h2 className="mb-4 text-lg font-semibold">Create Channel</h2>
                     <IoIosClose className="cursor-pointer" onClick={()=>setShowModal(false)} size={20}/>
@@ -64,7 +64,7 @@ const Modal = () => {
                         placeholder="# e.g plan-budget"
                         value={channelName}
                         onChange={(e) => setChannelName(e.target.value)}
-                        className={`w-full border rounded-md px-3 py-2 focus:outline-none focus:border-blue-500`}
+                        className={`w-full border rounded-md px-3 py-2 focus:outline-none focus:border-blue-500 dark:bg-[#222529]`}
                     />
                 </div>
                 <div className="mb-4">
@@ -78,7 +78,7 @@ const Modal = () => {
                             checked={visibility === "public"}
                             onChange={() => setVisibility("public")}
                         />
-                        <label htmlFor="public" className="text-gray-800">Public - anyone in New Workspace</label>
+                        <label htmlFor="public" className="text-gray-800 dark:text-gray-600">Public - anyone in New Workspace</label>
                     </div>
                     <div className="flex items-center mt-2">
                         <input
@@ -89,14 +89,14 @@ const Modal = () => {
                             checked={visibility === "private"}
                             onChange={() => setVisibility("private")}
                         />
-                        <label htmlFor="private" className="text-gray-800">Private - Only specific people</label>
+                        <label htmlFor="private" className="text-gray-800 dark:text-gray-600">Private - Only specific people</label>
                     </div>
                 </div>
                 <p className="mb-4 text-gray-500">Channels are where conversations happen around a topic. Use a name that is easy to find and understand.</p>
                 <div className="flex justify-between">
                     <div className="flex items-center">
                         <input type="checkbox" id="inviteExternal" className="mr-2" />
-                        <label htmlFor="inviteExternal" className="text-gray-800">Invite external people</label>
+                        <label htmlFor="inviteExternal" className="text-gray-800 dark:text-gray-600">Invite external people</label>
                     </div>
                     <button disabled={!channelName} onClick={handleCreateChannel} className={`${!channelName && 'bg-gray-400 cursor-not-allowed'} px-4 py-2 text-white bg-blue-500 rounded-md`}>
                         {loading? <RiLoader4Line /> : "Save"}
